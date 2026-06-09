c504f57ce1f0137335ddfb1e8e56c9afdd3ac440
LavieroMancinelli <mancinelli.laviero@gmail.com>
2026-06-08 20:32:07 -0700

    Added controls for agent type to main

--- src/main.py
+++ src/main.py
@@ -6,10 +6,11 @@ from renderer import Renderer
 from constants import BOARD_SIZE
 from solver import solver_step
 from baseline_solver import BaselineSolver
+import sys
 
 
 def run(mode="human", agent=None, fps=60):
-    env = Env(rows=BOARD_SIZE, cols=BOARD_SIZE, num_mines=5)
+    env = Env(rows=BOARD_SIZE, cols=BOARD_SIZE, num_mines=40)
     renderer = Renderer(env)
     baseline = BaselineSolver() if agent == "baseline" else None
 
@@ -35,4 +36,12 @@ def run(mode="human", agent=None, fps=60):
     renderer.close()
     
 if __name__ == "__main__":
-    run(mode="agent", agent="solver")
+    solver = sys.argv[1] if len(sys.argv) > 1 else None
+    if solver == "human":
+        run(mode="human")
+    elif solver == "solver":
+        run(mode="agent", agent="solver")
+    elif solver == "baseline":
+        run(mode="agent", agent="baseline")
+    else:
+        run(mode="human")
